47832b1342aae07dc468cb62b2aca687f9ae6bf9
Zhengyi Yang <zhengyi.yang@outlook.com>
2020-09-13 23:32:07 +1000

    add 4.2

--- 4.2-colorful-number.py
+++ 4.2-colorful-number.py
@@ -6,25 +6,25 @@ Created on Sat Sep 13 16:59:50 2020
 @author: zhengyi
 
 https://algorithms.tutorialhorizon.com/colorful-numbers/
+
+
+Objective: Given a number, find out whether its colorful or not.
+
+Colorful Number: When in a given number, product of every digit of a sub-sequence are different. That number is called Colorful Number. See Example
+
+Example:
+
+Given Number : 3245
+Output : Colorful
+Number 3245 can be broken into parts like 3 2 4 5 32 24 45 324 245.
+this number is a colorful number, since product of every digit of a sub-sequence are different.
+That is, 3 2 4 5 (3*2)=6 (2*4)=8 (4*5)=20, (3*2*4)= 24 (2*4*5)= 40
+
+Given Number : 326
+Output : Not Colorful.
+326 is not a colorful number as it generates 3 2 6 (3*2)=6 (2*6)=12.
+Reference : http://www.careercup.com/question?id=4863869499473920
 """
-
-
-# Objective: Given a number, find out whether its colorful or not.
-
-# Colorful Number: When in a given number, product of every digit of a sub-sequence are different. That number is called Colorful Number. See Example
-
-# Example:
-
-# Given Number : 3245
-# Output : Colorful
-# Number 3245 can be broken into parts like 3 2 4 5 32 24 45 324 245.
-# this number is a colorful number, since product of every digit of a sub-sequence are different.
-# That is, 3 2 4 5 (3*2)=6 (2*4)=8 (4*5)=20, (3*2*4)= 24 (2*4*5)= 40
-
-# Given Number : 326
-# Output : Not Colorful.
-# 326 is not a colorful number as it generates 3 2 6 (3*2)=6 (2*6)=12.
-# Reference : http://www.careercup.com/question?id=4863869499473920
 
 
 from functools import reduce
