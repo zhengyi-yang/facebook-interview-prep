13230f22d4a7f3c815e478b7347d9d9b2c402133
Zhengyi Yang <zhengyi.yang@outlook.com>
2020-10-21 23:33:35 +1100

    add mocks

--- Mocks/facebook-phone-interview.py
+++ Mocks/facebook-phone-interview.py
@@ -189,7 +189,56 @@ class GoatLatin:
 
         return ' '.join(result)
 
+
+class MinDepthBinaryTree:
+    '''
+    https://leetcode.com/problems/minimum-depth-of-binary-tree/
+    Definition for a binary tree node.
+        class TreeNode:
+            def __init__(self, val=0, left=None, right=None):
+                self.val = val
+                self.left = left
+                self.right = right
+    '''
+    from collections import deque
+
+    def minDepth(self, root) -> int:
+        if not root:
+            return 0
+
+        queue = deque()
+        queue.append((root, 1))
+
+        while queue:
+            node, depth = queue.popleft()
+            if not node.left and not node.right:
+                return depth
+
+            if node.left:
+                queue.append((node.left, depth+1))
+            if node.right:
+                queue.append((node.right, depth+1))
+
+
 # Medium
+
+class RandomPickIndex:
+    '''
+    https://leetcode.com/problems/random-pick-index/
+    '''
+    import random
+
+    def __init__(self, nums):
+        nums_dict = {}
+        for i, n in enumerate(nums):
+            if n in nums_dict:
+                nums_dict[n].append(i)
+            else:
+                nums_dict[n] = [i]
+        self.nums_dict = nums_dict
+
+    def pick(self, target: int) -> int:
+        return random.choice(self.nums_dict[target])
 
 
 class BulbSwitch:
